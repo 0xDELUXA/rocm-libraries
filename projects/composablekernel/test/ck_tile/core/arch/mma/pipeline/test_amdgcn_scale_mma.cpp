31d9f4d98b9c62948ce408c2e994354e4ddb0059
Yung-sheng Tu <yung-sheng@streamhpc.com>
2026-03-25 18:08:07 +0000

    add more tests for additional scale intrinsics

--- projects/composablekernel/test/ck_tile/core/arch/mma/pipeline/test_amdgcn_scale_mma.cpp
+++ projects/composablekernel/test/ck_tile/core/arch/mma/pipeline/test_amdgcn_scale_mma.cpp
@@ -32,39 +32,65 @@ using namespace ck_tile::core::arch::mma;
 
 using CompilerTargetGfx950 = decltype(make_amdgcn_gfx9_target<amdgcn_target_id::GFX950>());
 
+template <typename AType,
+          typename BType,
+          typename CType,
+          std::uint32_t WaveTileM,
+          std::uint32_t WaveTileN,
+          std::uint32_t WaveTileK>
+void ScaleMfmaGfx950Specialization_impl()
+{
+    using TestScaleMfma = amdgcn_mma<AType,
+                                     BType,
+                                     CType,
+                                     WaveTileM,
+                                     WaveTileN,
+                                     WaveTileK,
+                                     DefaultScaleMfmaCtrlFlags<AType, BType>,
+                                     CompilerTargetGfx950,
+                                     MmaOpFamily::SCALE>;
+
+    static_assert(std::is_same_v<typename TestScaleMfma::OpType, MfmaOp> &&
+                      TestScaleMfma::OpFamily == MmaOpFamily::SCALE,
+                  "GFX950 scale 16x16x128 should have ScaleMFMAOp type");
+
+    static_assert(is_mma_op_of_family_v<MmaOpFamily::SCALE, TestScaleMfma>,
+                  "GFX950 scale 16x16x128 should be detected as Scale");
+}
+
 TEST(ScaleMMATrait, ScaleMfmaGfx950Specialization)
 {
     // Test fp8 → fp32 scale MFMA for GFX950 (16x16x128)
-    using TestScaleMfma16x16 = amdgcn_mma<fp8_t,
-                                          fp8_t,
-                                          fp32_t,
-                                          16u,
-                                          16u,
-                                          128u,
-                                          DefaultScaleMfmaCtrlFlags<fp8_t, fp8_t>,
-                                          CompilerTargetGfx950,
-                                          MmaOpFamily::SCALE>;
-
-    static_assert(std::is_same_v<typename TestScaleMfma16x16::OpType, MfmaOp> &&
-                      TestScaleMfma16x16::OpFamily == MmaOpFamily::SCALE,
-                  "GFX950 scale 16x16x128 should have ScaleMFMAOp type");
-
-    static_assert(is_mma_op_of_family_v<MmaOpFamily::SCALE, TestScaleMfma16x16>,
-                  "GFX950 scale 16x16x128 should be detected as Scale");
+    ScaleMfmaGfx950Specialization_impl<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u>();
+    // Test bf8 → fp32 scale MFMA for GFX950 (16x16x128)
+    ScaleMfmaGfx950Specialization_impl<bf8_t, bf8_t, fp32_t, 16u, 16u, 128u>();
+    // Test fp4 → fp32 scale MFMA for GFX950 (16x16x128)
+    ScaleMfmaGfx950Specialization_impl<pk_fp4_t, pk_fp4_t, fp32_t, 16u, 16u, 128u>();
+    // Test fp8 → fp32 scale MFMA for GFX950 (32x32x64)
+    ScaleMfmaGfx950Specialization_impl<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u>();
+    // Test bf8 → fp32 scale MFMA for GFX950 (32x32x64)
+    ScaleMfmaGfx950Specialization_impl<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u>();
+    // Test fp4 → fp32 scale MFMA for GFX950 (32x32x64)
+    ScaleMfmaGfx950Specialization_impl<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u>();
 
     std::cout << "GFX950 scale MFMA specialization is correct" << std::endl;
 }
 
-TEST(ScaleMMATrait, MmaOpTraitsIntegration)
+template <typename AType,
+          typename BType,
+          typename CType,
+          std::uint32_t WaveTileM,
+          std::uint32_t WaveTileN,
+          std::uint32_t WaveTileK>
+void MmaOpTraitsIntegration_impl()
 {
-    // Create a scale MMA op (16x16x128 fp8 specialization)
-    using TestScaleMma = amdgcn_mma<fp8_t,
-                                    fp8_t,
-                                    fp32_t,
-                                    16u,
-                                    16u,
-                                    128u,
-                                    DefaultScaleMfmaCtrlFlags<fp8_t, fp8_t>,
+    using TestScaleMma = amdgcn_mma<AType,
+                                    BType,
+                                    CType,
+                                    WaveTileM,
+                                    WaveTileN,
+                                    WaveTileK,
+                                    DefaultScaleMfmaCtrlFlags<AType, BType>,
                                     CompilerTargetGfx950,
                                     MmaOpFamily::SCALE>;
 
@@ -76,55 +102,98 @@ TEST(ScaleMMATrait, MmaOpTraitsIntegration)
     static_assert(TestTraits::IsSupported, "Scale MMA specialization should be supported");
     static_assert(TestTraits::IsMfma, "Scale MFMA should be detected as MFMA");
     static_assert(!TestTraits::IsWmma, "Scale MFMA should not be detected as WMMA");
+}
+
+TEST(ScaleMMATrait, MmaOpTraitsIntegration)
+{
+    // Create a scale MMA op (16x16x128 fp8 specialization)
+    MmaOpTraitsIntegration_impl<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u>();
+    // Create a scale MMA op (16x16x128 bf8 specialization)
+    MmaOpTraitsIntegration_impl<bf8_t, bf8_t, fp32_t, 16u, 16u, 128u>();
+    // Create a scale MMA op (16x16x128 fp4 specialization)
+    MmaOpTraitsIntegration_impl<pk_fp4_t, pk_fp4_t, fp32_t, 16u, 16u, 128u>();
+    // Create a scale MMA op (32x32x64 fp8 specialization)
+    MmaOpTraitsIntegration_impl<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u>();
+    // Create a scale MMA op (32x32x64 bf8 specialization)
+    MmaOpTraitsIntegration_impl<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u>();
+    // Create a scale MMA op (32x32x64 fp4 specialization)
+    MmaOpTraitsIntegration_impl<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u>();
 
     std::cout << "MmaOpTraits correctly integrates scale operations" << std::endl;
 }
 
-TEST(ScaleMMATrait, TestConceptRequirements)
-{
 #if CK_TILE_CONCEPTS && CK_TILE_CONCEPTS_HEADER
-    using TestScaleMma = amdgcn_mma<fp8_t,
-                                    fp8_t,
-                                    fp32_t,
-                                    16u,
-                                    16u,
-                                    128u,
-                                    DefaultScaleMfmaCtrlFlags<fp8_t, fp8_t>,
+template <typename AType,
+          typename BType,
+          typename CType,
+          std::uint32_t WaveTileM,
+          std::uint32_t WaveTileN,
+          std::uint32_t WaveTileK>
+void TestConceptRequirements_impl()
+{
+    using TestScaleMma = amdgcn_mma<AType,
+                                    BType,
+                                    CType,
+                                    WaveTileM,
+                                    WaveTileN,
+                                    WaveTileK,
+                                    DefaultScaleMfmaCtrlFlags<AType, BType>,
                                     CompilerTargetGfx950,
                                     MmaOpFamily::SCALE>;
     static_assert(MmaOpI<TestScaleMma>);
+}
+#endif // CK_TILE_CONCEPTS && CK_TILE_CONCEPTS_HEADER
+
+TEST(ScaleMMATrait, TestConceptRequirements)
+{
+#if CK_TILE_CONCEPTS && CK_TILE_CONCEPTS_HEADER
+    TestConceptRequirements_impl<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u>();
+    TestConceptRequirements_impl<bf8_t, bf8_t, fp32_t, 16u, 16u, 128u>();
+    TestConceptRequirements_impl<pk_fp4_t, pk_fp4_t, fp32_t, 16u, 16u, 128u>();
+    TestConceptRequirements_impl<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u>();
+    TestConceptRequirements_impl<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u>();
+    TestConceptRequirements_impl<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u>();
 #else
     GTEST_SKIP() << "Not compiled with concepts. Skipping test.";
 #endif // CK_TILE_CONCEPTS && CK_TILE_CONCEPTS_HEADER
 }
 
+template <typename AType, typename BType, typename CType>
+void ScaleSelector_impl()
+{
+    static_for<2, 14, 6>{}([](auto k_factor) {
+        static_for<1, 33, 1>{}([&](auto i) {
+            using Selected                = typename MmaDefaultSelector<AType,
+                                                                        BType,
+                                                                        CType,
+                                                                        static_cast<std::uint32_t>(i),
+                                                                        static_cast<std::uint32_t>(i),
+                                                                        static_cast<std::uint32_t>(k_factor * i),
+                                                                        CompilerTargetGfx950,
+                                                                        MmaOpFamily::SCALE>::SelectedOp;
+            static constexpr bool isValid = (i == 16 && k_factor == 8) || (i == 32);
+            if constexpr(isValid)
+            {
+                // Selector should pick a scale MFMA implementation
+                static_assert(MmaOpTraits<Selected>::IsScale);
+                static_assert(MmaOpTraits<Selected>::IsMfma);
+                static_assert(MmaOpTraits<Selected>::IsSupported);
+                static_assert((std::is_same<typename Selected::OpType, MfmaOp>::value));
+            }
+            else
+            {
+                // Selector should pick the unsupported pass through
+                static_assert(!MmaOpTraits<Selected>::IsSupported);
+            }
+        });
+    });
+}
+
 TEST(ScaleMMATrait, ScaleSelector)
 {
-    static_for<1, 33, 1>{}([](auto i) {
-        using Selected = typename MmaDefaultSelector<fp8_t,
-                                                     fp8_t,
-                                                     fp32_t,
-                                                     static_cast<std::uint32_t>(i),
-                                                     static_cast<std::uint32_t>(i),
-                                                     static_cast<std::uint32_t>(8 * i),
-                                                     CompilerTargetGfx950,
-                                                     MmaOpFamily::SCALE>::SelectedOp;
-
-        static constexpr bool isValid = (i == 16) || (i == 32);
-        if constexpr(isValid)
-        {
-            // Selector should pick a scale MFMA implementation
-            static_assert(MmaOpTraits<Selected>::IsScale);
-            static_assert(MmaOpTraits<Selected>::IsMfma);
-            static_assert(MmaOpTraits<Selected>::IsSupported);
-            static_assert((std::is_same<typename Selected::OpType, MfmaOp>::value));
-        }
-        else
-        {
-            // Selector should pick the unsupported pass through
-            static_assert(!MmaOpTraits<Selected>::IsSupported);
-        }
-    });
+    ScaleSelector_impl<fp8_t, fp8_t, fp32_t>();
+    ScaleSelector_impl<bf8_t, bf8_t, fp32_t>();
+    ScaleSelector_impl<pk_fp4_t, pk_fp4_t, fp32_t>();
 }
 
 template <typename AType,
@@ -162,17 +231,23 @@ test_scale_accum_over_k(void* a, void* b, void* c, void* out, void* scale_A, voi
     *reinterpret_cast<CVecType*>(out) = result;
 }
 
-// Live test on real hardware for scale selection and execution.
-TEST(ScaleMMATrait, MmaSelector_Scale_F8_F8_F32_16x16x128_Real)
+template <typename AType,
+          typename BType,
+          typename CType,
+          std::uint32_t WaveTileM,
+          std::uint32_t WaveTileN,
+          std::uint32_t WaveTileK>
+void MmaSelector_Scale_Real_impl()
 {
-    using TestType = MmaPipelineTest<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u>;
+    using TestType = MmaPipelineTest<AType, BType, CType, WaveTileM, WaveTileN, WaveTileK>;
     TestType test;
     const auto should_skip = [](amdgcn_target_id currentArchId) {
         bool isSupportedWmma = false;
         bool isSupportedMfma = (currentArchId == amdgcn_target_id::GFX950);
         return ((currentArchId == amdgcn_target_id::HOST) || !(isSupportedWmma || isSupportedMfma));
     };
-    const std::function<fp32_t(std::uint32_t, TestType::ScaleAType, TestType::ScaleBType)>
+    const std::function<fp32_t(
+        std::uint32_t, typename TestType::ScaleAType, typename TestType::ScaleBType)>
         validator =
             [](std::uint32_t fragK, TestType::ScaleAType scale_A, TestType::ScaleBType scale_B) {
                 fp32_t actual_scale_A = std::powf(2.0f, scale_A - 127.0f);
@@ -186,15 +261,51 @@ TEST(ScaleMMATrait, MmaSelector_Scale_F8_F8_F32_16x16x128_Real)
                            void* out,
                            void* scale_A,
                            void* scale_B) {
-        test_scale_accum_over_k<TestType::AType,
-                                TestType::BType,
-                                TestType::CType,
-                                TestType::ScaleAType,
-                                TestType::ScaleBType,
+        test_scale_accum_over_k<typename TestType::AType,
+                                typename TestType::BType,
+                                typename TestType::CType,
+                                typename TestType::ScaleAType,
+                                typename TestType::ScaleBType,
                                 TestType::WaveTileM,
                                 TestType::WaveTileN,
                                 TestType::WaveTileK>
             <<<1, waveSize>>>(a, b, c, out, scale_A, scale_B);
     };
     test.test_pipeline(should_skip, kernel, validator);
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_F8_F8_F32_16x16x128_Real)
+{
+    MmaSelector_Scale_Real_impl<fp8_t, fp8_t, fp32_t, 16u, 16u, 128u>();
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_BF8_BF8_F32_16x16x128_Real)
+{
+    MmaSelector_Scale_Real_impl<bf8_t, bf8_t, fp32_t, 16u, 16u, 128u>();
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_F4_F4_F32_16x16x128_Real)
+{
+    MmaSelector_Scale_Real_impl<pk_fp4_t, pk_fp4_t, fp32_t, 16u, 16u, 128u>();
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_F8_F8_F32_32x32x64_Real)
+{
+    MmaSelector_Scale_Real_impl<fp8_t, fp8_t, fp32_t, 32u, 32u, 64u>();
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_BF8_BF8_F32_32x32x64_Real)
+{
+    MmaSelector_Scale_Real_impl<bf8_t, bf8_t, fp32_t, 32u, 32u, 64u>();
+}
+
+// Live test on real hardware for scale selection and execution.
+TEST(ScaleMMATrait, MmaSelector_Scale_F4_F4_F32_32x32x64_Real)
+{
+    MmaSelector_Scale_Real_impl<pk_fp4_t, pk_fp4_t, fp32_t, 32u, 32u, 64u>();
 }
